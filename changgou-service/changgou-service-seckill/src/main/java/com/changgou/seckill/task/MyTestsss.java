54704d9aa37e962eb56fbe16d8706592656d9f40
sanbai5 <13088888888@qq.com>
2019-11-14 21:31:11 +0800

    添加MyTestsss.java

---
+++ changgou-service/changgou-service-seckill/src/main/java/com/changgou/seckill/task/MyTestsss.java
@@ -0,0 +1,17 @@
+package com.changgou.seckill.task;
+
+/**
+ * @ClassName MyTestsss
+ * @Description
+ * @Author sanbai5
+ * @Date 21:29 2019/11/14
+ * @Version 2.1
+ **/
+public class MyTestsss {
+    public static void main(String[] args) {
+        if (1==1){
+            System.out.println("aaaaaaaaaaaaaaaaaa");
+        }
+
+    }
+}
